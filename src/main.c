a8bc89d114a8c1b0a709dd5754e0ab68a8d4f01f
brianlulei <lulei.wm@gmail.com>
2014-11-05 09:19:05 -0800

    Code clean-up

--- src/main.c
+++ src/main.c
@@ -23,130 +23,159 @@
 #include <stdlib.h>
 #include "hyperps/debug.h"
 
+#define BUFFER_SIZE 4096
+#define KERNEL_BASE 0xC0000000
+
 #ifndef DEBUG
 #define DEBUG 0
 #endif
 
 unsigned long init_task=0x16753a0;
-unsigned long offset_of_name;
-unsigned long offset_of_next;
+long offset_of_name;
+long offset_of_next;
 unsigned long offset_of_pid;
-void get_offsets(char *dumpfile);
+void get_offsets(FILE *dumpfile);
 void print_processes(char *dumpfile);
 
-int main()
+int main(int argc, char *argv[])
 {
-        DPRINTF("project kick-off!\n");
-	printf("==================Get Process Offset from the Trusted OS=======================\n");
-        get_offsets("dump.123");  // Get the offset of pid, process name, and next pointer
-	printf("===============================================================================\n");
-	printf("                                                                               \n");
-	printf("===============================================================================\n");
-	printf("==================Print Processes of the Monitoring OS=========================\n");
-        print_processes("dump.123"); // Print the process list of the monitoring Guest OS
+	if (argc != 2) {
+		printf("Usage: %s image_path\n", argv[0]);
+	} else {
+		DPRINTF("project kick-off!\n");
+		
+		FILE* dumpfile = fopen(argv[1], "rb");
+		if (!dumpfile) {
+       		printf("Unable to open dump file: %s\n", argv[1]);
+        	return -1;
+		}
+
+		printf("==================Get Process Offset from the Trusted OS=======================\n");	
+
+		get_offsets(dumpfile);  // Get the offset of pid, process name, and next pointer
+		fclose(dumpfile);
+
+		printf("===============================================================================\n");
+		printf("                                                                               \n");
+		printf("===============================================================================\n");
+		printf("==================Print Processes of the Monitoring OS=========================\n");
+		print_processes(argv[1]); // Print the process list of the monitoring Guest OS
+	}	
 	return 0;
 }
 
-//void get_offsets(char *dumpfile1, struct ps_info[])
-void get_offsets(char *dumpfile1)
+long get_offset_of_name(FILE * fd, char* buffer, char* target_name) 
 {
-	FILE *ptr_dumpfile;
-	unsigned char ptr[4096];	// When we calculate the offset of process name, we call fread and store the output in ptr[]
-	unsigned long ptr_next[1];	// When we calculate the offset of next pointer, we call fread and store the output in ptr_next[]
-	unsigned long ptr_pid[1];	// When we calculate the offset of pid, we call fread and store the output in ptr_pid[]
-	unsigned long ptr_next_next[1];	// When we calculate the offset of pid, we need to compare one more time, since the first pid is 0, 
-					// the second pid is 1, they appears in the memory too often, therefore we go one more step and verify the next pid is 2.
-	unsigned long ptr_pid_next[1];
-	unsigned long read_counter=0;
-	void * ptr_name;
-	char *name="swapper";		// This is pid=0 process, in Linux.
-	char *name1="init";		// This is the pid=1 process, in Linux.
-	ptr_dumpfile=fopen(dumpfile1,"rb");
-	if (!ptr_dumpfile)
-  	{
-       		printf("Unable to open dump file!\n");
-        	exit(1);
-        }
+	long read_counter = 0;
+	long offset = -1;
+	char* name_pos = NULL;
 
-	/* Compute the offset of the process name */
-	fseek(ptr_dumpfile,init_task,SEEK_SET);
-	while( !feof(ptr_dumpfile) )
-	{
-		fread(ptr,4096,1,ptr_dumpfile);
-		if( (memmem(ptr,4096,name,strlen(name)) != NULL) ) 
+	fseek(fd, init_task, SEEK_SET);
+	
+	while (!feof(fd)) {
+		fread(buffer, BUFFER_SIZE, 1, fd);
+		name_pos = (char*) memmem(buffer, BUFFER_SIZE, target_name, strlen(target_name));
+		if (name_pos != NULL)
 			break;
 		read_counter++;
 	}
-	ptr_name=memmem(ptr,4096,name,strlen(name));
-	if(ptr_name==NULL)
-	{
-		printf("Couldn't find the process name!\n");
-		fclose(ptr_dumpfile);
-		exit(1);
-	}
-	offset_of_name=(void *)ptr_name-(void *)ptr+read_counter*4096;
-	printf("Offset of process name is: 0x%lx\n", offset_of_name);
 
-	/* Compute the offset of the next pointer */
-	read_counter=0;
-	fseek(ptr_dumpfile,init_task,SEEK_SET);
-        while( !feof(ptr_dumpfile) )
-        {
-                fread(ptr_next,4,1,ptr_dumpfile);
-		unsigned long ptr_mark=ftell(ptr_dumpfile);
-		if(*ptr_next>0xc0000000)
-		{
-//			printf("Value of next pointer is: 0x%lx\n", *ptr_next);
-			offset_of_next=read_counter*4;
-			fseek(ptr_dumpfile,*ptr_next-0xc0000000-offset_of_next,SEEK_SET);
-			fread(ptr,4096,1,ptr_dumpfile);
-			if( (void *)memmem(ptr,4096,name1,strlen(name1)) - (void *)ptr == offset_of_name )
-			{
+	if (name_pos == NULL) {
+		printf("Couldn't find the process name!\n");
+		fclose(fd);
+		return -1;
+	}
+
+	offset = name_pos - buffer + read_counter * BUFFER_SIZE;
+	return offset;
+}
+
+long get_offset_of_next(FILE* fd, char* ptr_next, char* buffer, char* process_1) {
+	long read_counter = 0;
+	unsigned long ptr_mark = 0;
+	long offset = -1;
+
+	fseek(fd, init_task, SEEK_SET);
+
+	while (!feof(fd)) {
+		// read another 4 bytes and assume it is a pointer
+		fread(ptr_next, 4, 1, fd);
+		ptr_mark = ftell(fd);
+
+		//if the pointer points to the kernel space
+		if (*((unsigned long*) ptr_next) > KERNEL_BASE) {
+			offset = read_counter * 4;
+			fseek(fd, *((unsigned long*)ptr_next) - KERNEL_BASE - offset, SEEK_SET);
+			fread(buffer, BUFFER_SIZE, 1, fd);
+			if ((void *)memmem(buffer, BUFFER_SIZE, process_1, strlen(process_1)) - (void *)buffer == offset_of_name) {
 				printf("Yes we found the next process name\n");
-				printf("Therefore, we assert that the offset of the next pointer is: 0x%lx\n", offset_of_next);
+				printf("Therefore, we assert that the offset of the next pointer is: 0x%lx\n", offset);
 				break;
 			}
 		}
-                read_counter++;
-		fseek(ptr_dumpfile,ptr_mark,SEEK_SET);	// We need to go back to the original location and continue our fread.
-//	if(read_counter>120)
-//	break;
-        }
 
-	/* Compute the offset of the pid */
-	read_counter=0;
-	fseek(ptr_dumpfile,init_task,SEEK_SET);
-        while( !feof(ptr_dumpfile) )
-        {
-                fread(ptr_pid,4,1,ptr_dumpfile);
-		unsigned long ptr_mark=ftell(ptr_dumpfile);
-		if(*ptr_pid==0)
-		{
-			offset_of_pid=read_counter*4;
-			fseek(ptr_dumpfile,*ptr_next-0xc0000000-offset_of_next+offset_of_pid,SEEK_SET);
-                	fread(ptr_pid,4,1,ptr_dumpfile);
-			if(*ptr_pid==1)
-			{
-				fseek(ptr_dumpfile,*ptr_next-0xc0000000,SEEK_SET);
-				fread(ptr_next_next,4,1,ptr_dumpfile);
-				fseek(ptr_dumpfile,*ptr_next_next-0xc0000000-offset_of_next+offset_of_pid,SEEK_SET);
-                		fread(ptr_pid_next,4,1,ptr_dumpfile);
-				if(*ptr_pid_next==2)
-				{
+        read_counter++;
+		fseek(fd, ptr_mark, SEEK_SET);	// We need to go back to the original location and continue our fread.
+    }
+	return offset;
+}
+
+long get_offset_of_pid (FILE *fd, char* ptr_next, char *ptr_pid, char *ptr_next_next, char *ptr_pid_next) {
+	long read_counter = 0;
+	long offset = -1;
+
+	fseek(fd, init_task, SEEK_SET);
+
+    while (!feof(fd)) {
+		fread(ptr_pid, 4, 1, fd);
+		unsigned long ptr_mark = ftell(fd);
+		if (*((unsigned long*)ptr_pid) == 0) {
+			offset = read_counter * 4;
+			fseek(fd, *((unsigned long*)ptr_next) - KERNEL_BASE - offset_of_next + offset, SEEK_SET);
+            fread(ptr_pid, 4, 1, fd);
+		
+			if (*((unsigned long*)ptr_pid) == 1) {
+				fseek(fd, *((unsigned long*)ptr_next) - KERNEL_BASE, SEEK_SET);
+				fread(ptr_next_next, 4, 1, fd);
+				fseek(fd, *((unsigned long*)ptr_next_next) - KERNEL_BASE - offset_of_next + offset, SEEK_SET);
+                fread(ptr_pid_next, 4, 1, fd);
+
+				if (*((unsigned long*)ptr_pid_next) == 2) {
 					printf("Yes we found the next pid.\n");
-					printf("Therefore, we assert that the offset of the pid is: 0x%lx\n", offset_of_pid);
+					printf("Therefore, we assert that the offset of the pid is: 0x%lx\n", offset);
 					break;
 				}
 			}
 		}
 		read_counter++;	
-		fseek(ptr_dumpfile,ptr_mark,SEEK_SET);	// We need to go back to the original location and continue our fread.
-//	if(read_counter>120)
-//	break;
+		fseek(fd, ptr_mark, SEEK_SET);	// We need to go back to the original location and continue our fread.
 	}
+	return offset;
+}
 
-	fclose(ptr_dumpfile);
+void get_offsets(FILE *dumpfile)
+{
+	char ptr[BUFFER_SIZE];	// When we calculate the offset of process name, we call fread and store the output in ptr[]
+	char ptr_next[4];	// When we calculate the offset of next pointer, we call fread and store the output in ptr_next[]
+	char ptr_pid[4];	// When we calculate the offset of pid, we call fread and store the output in ptr_pid[]
+	char ptr_next_next[4];	// When we calculate the offset of pid, we need to compare one more time, since the first pid is 0, 
+					// the second pid is 1, they appears in the memory too often, therefore we go one more step and verify the next pid is 2.
+	char ptr_pid_next[4];
+	char *name = "swapper";		// This is pid=0 process, in Linux.
+	char *name1 = "init";		// This is the pid=1 process, in Linux.
 
+
+	/* Compute the offset of the process name */
+	offset_of_name = get_offset_of_name(dumpfile, ptr, name);
+	printf("Offset of process name is: 0x%lx\n", offset_of_name);
+
+	/* Compute the offset of the next pointer */
+	offset_of_next = get_offset_of_next(dumpfile, ptr_next, ptr, name1);
+	printf("Offset of next pointer is: 0x%lx\n", offset_of_next);
+
+	/* Compute the offset of the pid */
+	offset_of_pid = get_offset_of_pid(dumpfile, ptr_next, ptr_pid, ptr_next_next, ptr_pid_next);
+	printf("Offset of pid is: 0x%lx\n", offset_of_pid);
 }
 
 void print_processes(char *dumpfile2)
@@ -155,37 +184,35 @@ void print_processes(char *dumpfile2)
 	unsigned char ptr[4096];
 	unsigned long ptr_next[1];
 	unsigned long ptr_pid[1];
-	unsigned long print_counter=0;
-	ptr_dumpfile=fopen(dumpfile2,"rb");
-        if (!ptr_dumpfile)
-        {
-                printf("Unable to open dump file!\n");
-                exit(1);
-        }
+	unsigned long print_counter = 0;
+	ptr_dumpfile = fopen(dumpfile2,"rb");
+	if (!ptr_dumpfile) {
+		printf("Unable to open dump file!\n");
+		exit(1);
+    }
 
 	printf("PID PROCESS_NAME\n");
-	fseek(ptr_dumpfile,init_task,SEEK_SET);
+	fseek(ptr_dumpfile, init_task, SEEK_SET);
 //	printf("%lx\n",init_task+offset_of_next);
 	while(1)
 	{
 		unsigned long ptr_mark=ftell(ptr_dumpfile);
-		fseek(ptr_dumpfile,ptr_mark+offset_of_pid,SEEK_SET);
-               	fread(ptr_pid,4,1,ptr_dumpfile);
-		*ptr_pid=*ptr_pid&0xffffffff;
+		fseek(ptr_dumpfile, ptr_mark + offset_of_pid, SEEK_SET);
+        fread(ptr_pid, 4, 1, ptr_dumpfile);
+		*ptr_pid = *ptr_pid & 0xffffffff;
 		printf("%ld ",*ptr_pid);
-		fseek(ptr_dumpfile,ptr_mark+offset_of_name,SEEK_SET);
-		fread(ptr,4096,1,ptr_dumpfile);
+		fseek(ptr_dumpfile, ptr_mark + offset_of_name, SEEK_SET);
+		fread(ptr, 4096, 1, ptr_dumpfile);
 		printf("%s\n",ptr);
-		fseek(ptr_dumpfile,ptr_mark+offset_of_next,SEEK_SET);
-                fread(ptr_next,4,1,ptr_dumpfile);
-		*ptr_next=*ptr_next&0xffffffff;
+		fseek(ptr_dumpfile, ptr_mark + offset_of_next, SEEK_SET);
+        fread(ptr_next, 4, 1, ptr_dumpfile);
+		*ptr_next = *ptr_next & 0xffffffff;
 //		printf(" %lx\n",(*ptr_next));
-		if( *ptr_next == (init_task+offset_of_next+0xc0000000) )
+		if( *ptr_next == (init_task + offset_of_next + 0xc0000000) )
 			break;
-		fseek(ptr_dumpfile,*ptr_next-0xc0000000-offset_of_next,SEEK_SET);
+		fseek(ptr_dumpfile, *ptr_next - 0xc0000000 - offset_of_next, SEEK_SET);
 		print_counter++;
 	}
-		printf("Total number of processes: %ld\n", print_counter);
+	printf("Total number of processes: %ld\n", print_counter);
 	fclose(ptr_dumpfile);
 }
-
